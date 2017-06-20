f2af1fe2b6f19774479a478397d1d80461add713
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-06-20 11:40:38 +0200

    Added Proof of Concept code

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from git import Repo
+from typing import List
+
+from git import Repo, Diff
 
 repo = Repo('C:\\Users\\Pascal\\Source\\AutoGit')
 
-repo2 = repo
+for untracked_file in repo.untracked_files:
+    print(untracked_file)
+
+
+def changedWorkingDir() -> List[Diff]:
+    return repo.head.commit.diff(None)
+
+for diff in changedWorkingDir():
+    print(str(diff.b_path) + " " + diff.change_type)
