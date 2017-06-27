5e17f4c7929ee7977555130b2b1f90860b376aaf
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-06-27 10:02:27 +0200

    Command Line Arguments parsen und für verwenden

--- configurator.py
+++ configurator.py
@@ -1,24 +1,25 @@
 import json
 import os
 
-class requirements():
+
+class Requirements:
     def __init__(self, data, config_id):
-            self.deleted_files = data["settings"][config_id]["commit-requirements"]["deleted-files"]
-            self.changed_files = data["settings"][config_id]["commit-requirements"]["changed-files"]
-            self.new_files = data["settings"][config_id]["commit-requirements"]["new-files"]
-            self.changed_lines = data["settings"][config_id]["commit-requirements"]["changed-lines"]
+        self.deleted_files = data["settings"][config_id]["commit-requirements"]["deleted-files"]
+        self.changed_files = data["settings"][config_id]["commit-requirements"]["changed-files"]
+        self.new_files = data["settings"][config_id]["commit-requirements"]["new-files"]
+        self.changed_lines = data["settings"][config_id]["commit-requirements"]["changed-lines"]
 
 
-class config():
+class Config:
     def __init__(self, config_id):
         self.load_data(config_id)
 
     def load_data(self, config_id):
-        file_path = os.getcwd()+"\config.json"
+        file_path = os.getcwd() + "\config.json"
         with open(file_path) as data_file:
             self.data = json.load(data_file)
             self.config_name = self.data["settings"][config_id]["config-name"]
             self.auto_commit = self.data["settings"][config_id]["auto-commit"]
             self.commit_message = self.data["settings"][config_id]["commit-message"]
             self.frequenz = self.data["settings"][config_id]["frequenz"]
-            self.requirements = requirements(self.data, config_id)
+            self.requirements = Requirements(self.data, config_id)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import sys
-from automation import Automation
+import os
+from optparse import OptionParser
 from typing import List
+
 from git import Repo, Diff
+
+from automation import Automation
+from configurator import Config
 
 
 class Main:
@@ -16,10 +20,32 @@ class Main:
         for diff in self.changed_working_dir:
             print(str(diff.b_path) + " " + diff.change_type)
 
-    def __init__(self, path):
-        self.repo = Repo(path)
+    def __init__(self, configuration, repo_path):
+        config = Config(configuration)
+
+        self.repo = Repo(repo_path)
 
         Automation(self.check_untracked_files, 2)
 
+parser = OptionParser()
+parser.add_option("-r", "--repository",
+                  dest="repository", help="GIT Repository to use")
+parser.add_option("-c", "--config", dest="config",
+                  help="Configuration ID to use", metavar="FILE")
 
-main = Main(sys.argv[1])
+(options, args) = parser.parse_args()
+
+configuration = options.config
+repository = options.repository
+
+if not configuration:
+    # TODO Print available configs
+    configuration = input("Configuration: ")
+
+if not repository:
+    repository = os.getcwd()
+
+print("Config: " + configuration)
+print("Repo: " + repository)
+
+main = Main(int(configuration), repository)
