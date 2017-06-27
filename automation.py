7872eda95a4b14c0897d70be70b5e8fd08e28cda
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-06-27 09:16:33 +0200

    Made Alains code Pascals again

--- automation.py
+++ automation.py
@@ -1,15 +1,17 @@
 import sched, time
 
+
 class Automation:
 
-    def __init__(self):
+    def __init__(self, action, timeout_in_seconds):
+        self.action = action
         self.s = sched.scheduler(time.time, time.sleep)
-        self.s.enter(1, 1, self.do_something)
+        self.s.enter(timeout_in_seconds, 1, self.execute_action)
         self.s.run()
 
-    def do_something(self):
-        print("Doing stuff...")
-        # do your stuff
-        self.s.enter(1, 1, self.do_something)
+    def execute_action(self):
+        print("Executing action...")
 
-auto = Automation()
+        self.action()
+
+        self.s.enter(1, 1, self.execute_action)
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import sys
+from automation import Automation
 from typing import List
 from git import Repo, Diff
 
 
 class Main:
     @property
-    def changedWorkingDir(self) -> List[Diff]:
+    def changed_working_dir(self) -> List[Diff]:
         return self.repo.head.commit.diff(None)
+
+    def check_untracked_files(self):
+        for untracked_file in self.repo.untracked_files:
+            print(untracked_file)
+
+        for diff in self.changed_working_dir:
+            print(str(diff.b_path) + " " + diff.change_type)
 
     def __init__(self, path):
         self.repo = Repo(path)
 
-        for untracked_file in self.repo.untracked_files:
-            print(untracked_file)
-
-        for diff in self.changedWorkingDir:
-            print(str(diff.b_path) + " " + diff.change_type)
+        Automation(self.check_untracked_files, 2)
 
 
 main = Main(sys.argv[1])
