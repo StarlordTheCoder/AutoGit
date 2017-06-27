3c635a6dc62b95888d2d4a97a6dc9fd35393dfa5
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-06-27 08:53:00 +0200

    Code um Klasse erweitert

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import sys
 from typing import List
-
 from git import Repo, Diff
 
-repo = Repo('C:\\Users\\Pascal\\Source\\AutoGit')
 
-for untracked_file in repo.untracked_files:
-    print(untracked_file)
+class Main:
+    @property
+    def changedWorkingDir(self) -> List[Diff]:
+        return self.repo.head.commit.diff(None)
+
+    def __init__(self, path):
+        self.repo = Repo(path)
+
+        for untracked_file in self.repo.untracked_files:
+            print(untracked_file)
+
+        for diff in self.changedWorkingDir:
+            print(str(diff.b_path) + " " + diff.change_type)
 
 
-def changedWorkingDir() -> List[Diff]:
-    return repo.head.commit.diff(None)
-
-for diff in changedWorkingDir():
-    print(str(diff.b_path) + " " + diff.change_type)
+main = Main(sys.argv[1])
