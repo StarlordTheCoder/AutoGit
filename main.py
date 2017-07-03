225f355e1eda6f119805e02c737ec1bba9650336
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-07-03 21:30:49 +0200

    Only commit if limits exceeded

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ from automation import Automation
 
 class Main:
     def check_untracked_files(self):
-        hasExceededLimit = False
+        has_exceeded_limit = False
 
         if len(self.repo.untracked_files) >= self.config.requirements.untracked_files:
-            hasExceededLimit = True
+            has_exceeded_limit = True
+        else:
+            git_diff_string = self.repo.git.diff(shortstat=True)
 
-        print(self.repo.git.diff(shortstat=True))
-        # TODO Consider added, modified and removed files
-        if hasExceededLimit:
-            self.repo.git.add(A = True)
-            self.repo.git.commit(m = self.config.commit_message)
+            split_diff = git_diff_string.split(", ")
+
+            print(split_diff)
+
+            changed_files = int(split_diff[0].replace(" files changed", ""))
+            insertions = int(split_diff[1].replace(" insertions(+)", ""))
+            deletions = int(split_diff[2].replace(" deletions(-)", ""))
+            
+            if changed_files > self.config.requirements.changed_files or insertions > self.config.requirements.added_lines or deletions > self.config.requirements.removed_lines:
+                has_exceeded_limit = True
+
+        if has_exceeded_limit:
+            self.repo.git.add(A=True)
+            self.repo.git.commit(m=self.config.commit_message)
 
     def __init__(self, config_manager, configuration, repo_path):
         self.config = config_manager.get_config(configuration)
